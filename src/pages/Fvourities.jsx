f1bd0bb03b5135830b87cfc888aa9ec548e2e24f
bigifx9 <achukechinemeremfelix@gmail.com>
2025-12-29 20:45:50 -0800

    myfirstcommit

---
+++ src/pages/Fvourities.jsx
@@ -0,0 +1,32 @@
+import{ useMovieContext } from "../context/Moviecontext"
+import Movieapp from "../components/Movieapp"
+
+function Favourities(){
+
+  const {kosi}= useMovieContext();
+
+  if (kosi) {
+
+      return (
+      <div className="my-favourities-page">
+            <h2>YOUR FAVOURITES</h2>
+      <div className="movie-list">
+      {kosi.map((movie) =>(
+            <Movieapp movie={movie} key={movie.id} />))}
+        
+     </div>
+     </div>)
+     }
+      
+     
+      return (
+            <div className="favourities-page">
+                  <h1>No Favourities Movie Yet</h1>
+                  <p> Start Adding Movies To Your Favourities So That They Will Show Here</p>
+            </div>
+      )
+  }
+      
+     
+
+export default Favourities;
